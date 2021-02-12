e227acf39521f33e8e8ea622e7d1c5a6c6d46698
Tangil <21197843+TheBlocks@users.noreply.github.com>
2021-02-12 13:36:06 +0000

    Fix type declaration conflict with stdint

--- easywsclient.cpp
+++ easywsclient.cpp
@@ -30,7 +30,7 @@
         // vs2010 or later
         #include <stdint.h>
     #else
-        typedef __int8 int8_t;
+        typedef __int8 _int8_t;
         typedef unsigned __int8 uint8_t;
         typedef __int32 int32_t;
         typedef unsigned __int32 uint32_t;
